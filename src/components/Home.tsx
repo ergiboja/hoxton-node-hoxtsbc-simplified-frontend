e194a1f31f9fc4763903b5d5ba0c0246079dc18c
Ergi  Boja <perdoruesperdoruesi@gmail.com>
2022-09-22 02:21:26 +0200

    commit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,38 @@
 import { User } from "../App"
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Navigate, Route, } from "react-router-dom";
+import logo from './logo.svg'
+import { Routes } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+
 
 
 function Home({signout}){
+    const [transactions,setTransactions]=useState<any[]>([])
+
+
+
+    useEffect(()=>{
+        fetch('http://localhost:4000/transactions')
+        .then((response) => response.json())
+        .then((data) => setTransactions(data));
+    },[]);
+    
    
     return(
         <>
-        <h1>homeee</h1>
+        <h1>Welcome</h1>
+        {transactions.map((transaction)=> <div>   
+        <h3>Payment</h3>
+        <p>{transaction.transaction_name}</p>
+        <h3>Ammount</h3>
+        <p>{transaction.transaction_ammount}$</p>
+        </div>
+            
+            
+            
+            )}
+       
         <button className="btn"onClick={signout}>Sign Out</button>
         </>
     )
